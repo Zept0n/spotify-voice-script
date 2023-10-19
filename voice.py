ab5012cafcaed11adfdcc4269e0408b7b6c153d5
Zept0n <104791727+Zept0n@users.noreply.github.com>
2023-10-19 18:58:05 +0300

    added tray icon

--- voice.py
+++ voice.py
@@ -1,64 +1,83 @@
 import speech_recognition as sr
-import pyaudio
-import sys
 from spotify_control import SpotifyControl
-import keyboard
+import pyaudio
+import pystray
+from PIL import Image
+import sys
+import threading
 
+exiting = False
 
-global exiting
-exiting=False
-
-def exit_script():
+def exit_script(icon, item):
     global exiting
-    exiting=True
+    exiting = True
     print("Exiting the script.")
-    sys.exit()
+    icon.stop()
 
-try:
-    # start listening for voice commands
-    play_commands = ['play','pause','resume','stop song','start song','stop','false']
-    previous_commands =['previous','last']
-    next_commands=['next']
-    volumeup_commands=['volume up','volumeup','up']
-    volumedown_commands=['volume down','volumedown','down']
-    control=SpotifyControl()
-    r = sr.Recognizer()
-    m= sr.Microphone()
-    print("Listening...")
-    with m as source:
-            r.adjust_for_ambient_noise(source,duration=2) # ambient noise adjust
-            keyboard.add_hotkey('ctrl+x', exit_script)
-            while exiting == False:  # loop to continuously listen for commands
-                try:
-                    audio = r.listen(source, phrase_time_limit=3)
-                except sr.WaitTimeoutError as e:
-                    print(f"Timeout error: {e}")
-                    continue  # Continue the loop when a timeout error occurs
+def on_exit(exit_code):
+    if exiting:
+        sys.exit(exit_code)
 
-                if audio:
+def tray_icon():
+    # Create a tray icon
+        image = Image.open("favicon.ico") 
+        menu = (pystray.MenuItem('Exit', exit_script),)
+
+        icon = pystray.Icon('name',image, 'Spotify Control',menu)
+
+        icon_thread = threading.Thread(target=icon.run)
+        icon_thread.daemon = True
+        icon_thread.start()
+
+def main():
+    # Your initialization code here
+    play_commands = ['play', 'pause', 'resume', 'stop song', 'start song', 'stop', 'start', 'false']
+    previous_commands = ['previous', 'last']
+    next_commands = ['next']
+    volumeup_commands = ['volume up', 'volumeup', 'up']
+    volumedown_commands = ['volume down', 'volumedown', 'down']
+    try:
+        control = SpotifyControl()
+        r = sr.Recognizer()
+        m = sr.Microphone()
+        tray_icon()
+        with m as source:
+                r.adjust_for_ambient_noise(source,duration=2) # ambient noise adjust
+                while exiting == False:  # loop to continuously listen for commands
                     try:
-                        command = r.recognize_google(audio).lower()
-                        print(f"You said: {command}")
+                        audio = r.listen(source, phrase_time_limit=3)
+                    except sr.WaitTimeoutError as e:
+                        print(f"Timeout error: {e}")
+                        continue  # Continue the loop when a timeout error occurs
 
-                        if any(word in command for word in play_commands):
-                            control.play_pause()
-                        elif any(word in command for word in previous_commands):
-                            control.previous()
-                        elif any(word in command for word in next_commands):
-                            control.next()
-                        elif any(word in command for word in volumeup_commands):
-                            control.volumeup()
-                        elif any(word in command for word in volumedown_commands):
-                            control.volumedown()
+                    if audio:
+                        try:
+                            command = r.recognize_google(audio).lower()
+                            print(f"You said: {command}")
 
-                    except sr.UnknownValueError:
-                        #print("Google Speech Recognition could not understand audio")
-                        continue
-                    except sr.RequestError as e:
-                        print(f"Could not request results from Google Speech Recognition service; {e}")
-                        continue
-                    except pyaudio.paBadStreamPtr:
-                        # Ignore this exception if the program is exiting
-                        raise
-except Exception as e:
-    sys.exit()
+                            if any(word in command for word in play_commands):
+                                control.play_pause()
+                            elif any(word in command for word in previous_commands):
+                                control.previous()
+                            elif any(word in command for word in next_commands):
+                                control.next()
+                            elif any(word in command for word in volumeup_commands):
+                                control.volumeup()
+                            elif any(word in command for word in volumedown_commands):
+                                control.volumedown()
+
+                        except sr.UnknownValueError:
+                            #print("Google Speech Recognition could not understand audio")
+                            continue
+                        except sr.RequestError as e:
+                            print(f"Could not request results from Google Speech Recognition service; {e}")
+                            continue
+                        except pyaudio.paBadStreamPtr:
+                            # Ignore this exception if the program is exiting
+                            raise
+    except Exception as e:
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
